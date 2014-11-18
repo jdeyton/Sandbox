0772457197db8ab3c7eb7eb9c4dccd87ac9cba23
Jordan Deyton <deytonjh@ornl.gov>
2014-11-18 12:40:00 -0500

    Restructured the test application a bit to facilitate sub-classing it.

--- src/com.bar.foo.javafx/src/com/bar/foo/javafx/app/TestApplication.java
+++ src/com.bar.foo.javafx/src/com/bar/foo/javafx/app/TestApplication.java
@@ -8,23 +8,51 @@ import javafx.stage.Stage;
 
 public class TestApplication extends Application {
 
+	private final Scene scene;
+	private final Group root;
+
+	public TestApplication() {
+		root = new Group();
+		scene = new Scene(root, Color.BLACK);
+	}
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
 		// Configure the default, initial scene.
-		Group root = new Group();
-		Scene scene = new Scene(root, Color.BLACK);
 		primaryStage.setScene(scene);
+
+		primaryStage.setTitle("Test Application");
 
 		// Configure the size of the Application window.
 		primaryStage.setResizable(false);
 		primaryStage.setWidth(1024);
 		primaryStage.setHeight(768);
 
+		// Initialize the application.
+		initScene();
+
 		// Opens the Application window.
 		primaryStage.show();
 
 		return;
+	}
+
+	@Override
+	public void stop() throws Exception {
+		// Uninitialize the application.
+		disposeScene();
+
+		// Proceed with the normal stop procedure.
+		super.stop();
+	}
+
+	public void initScene() {
+		// TODO
+	}
+
+	public void disposeScene() {
+		// TODO
 	}
 
 }
