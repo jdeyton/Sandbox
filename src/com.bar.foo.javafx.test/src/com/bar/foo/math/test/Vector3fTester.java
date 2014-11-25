04ae4417665079aa432a4c51db9a950d937a54f9
Jordan Deyton <jordan.deyton@gmail.com>
2014-11-25 18:56:36 -0500

    Implemented test for distance calculation.

--- src/com.bar.foo.javafx.test/src/com/bar/foo/math/test/Vector3fTester.java
+++ src/com.bar.foo.javafx.test/src/com/bar/foo/math/test/Vector3fTester.java
@@ -369,7 +369,7 @@ public class Vector3fTester {
 		vector.normalize();
 		assertEquals(1f, vector.lengthSquared(), delta);
 		assertEquals(1f, vector.length(), delta);
-		
+
 		return;
 	}
 
@@ -726,7 +726,7 @@ public class Vector3fTester {
 		// ---- Check that the two cache-based ops handle null cache. ---- //
 		// Clear the cache so it is not unintentionally used again in this.
 		cache.set(1f, 2f, 3f);
-		
+
 		// Reset the vector to the x, y, and z values.
 		vector.set(x, y, z);
 
@@ -766,19 +766,67 @@ public class Vector3fTester {
 	}
 
 	/**
-	 * TODO
+	 * Checks the computations of distance between two vectors. A very simple
+	 * case and a random case are both tested for good measure.
+	 * 
+	 * @see Vector3f#distance(Vector3f)
+	 * @see Vector3f#distanceSquared(Vector3f)
 	 */
 	@Test
 	public void checkDistance() {
-		// Generate some random coordinates. Since float is a primitive type, we
-		// don't have to worry about pass-by-reference affecting test results.
+		Vector3f vector = new Vector3f();
+		Vector3f vector2 = new Vector3f();
+		Vector3f nullVector = null;
+
+		// The simple case is the distance between opposite points on the unit
+		// cube.
+		float distance = (float) Math.sqrt(3.0);
+		float distanceSquared = 3f;
+
+		// Since both methods take a vector argument, use the cache.
+		vector.set(Vector3f.ZERO);
+		vector2.set(Vector3f.IDENTITY);
+		// Check the return values of the two methods.
+		assertEquals(distanceSquared, vector.distanceSquared(vector2), delta);
+		assertEquals(distance, vector.distance(vector2), delta);
+
+		// Now try a more complicated example with random values.
 		float x = random.nextFloat();
 		float y = random.nextFloat();
 		float z = random.nextFloat();
+		float x2 = random.nextFloat();
+		float y2 = random.nextFloat();
+		float z2 = random.nextFloat();
+		// Determine the distance.
+		double dx = (x2 - x);
+		double dy = (y2 - y);
+		double dz = (z2 - z);
+		distanceSquared = (float) (dx * dx + dy * dy + dz * dz);
+		distance = (float) Math.sqrt(dx * dx + dy * dy + dz * dz);
+		// Update the two vectors involved.
+		vector.set(x, y, z);
+		vector2.set(x2, y2, z2);
+		// Check the two methods.
+		assertEquals(distanceSquared, vector.distanceSquared(vector2), delta);
+		assertEquals(distance, vector.distance(vector2), delta);
 
-		Vector3f vector = new Vector3f();
-
-		fail("Not implemented.");
+		// ---- Check for null pointer exceptions. ---- //
+		// These should only happen when a non-cache vector is passed.
+		try {
+			vector.distanceSquared(nullVector);
+			fail(failurePrefix
+					+ "Operation supports null vector argument for vector that is not used as a cache!");
+		} catch (NullPointerException e) {
+			// Nothing to do.
+		}
+		try {
+			vector.distance(nullVector);
+			fail(failurePrefix
+					+ "Operation supports null vector argument for vector that is not used as a cache!");
+		} catch (NullPointerException e) {
+			// Nothing to do.
+		}
+		// -------------------------------------------- //
 
 		return;
 	}
