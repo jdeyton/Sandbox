591910645ef5d5286f77dabad7af7c3f82aca7f1
Jordan Deyton <deytonjh@ornl.gov>
2015-04-22 14:46:36 -0400

    Adding some just-for-fun python code created with PyDev.

---
+++ python/src/com.bar.foo.pyground/com/bar/foo/pyground/fun.py
@@ -0,0 +1,22 @@
+'''
+Created on Apr 22, 2015
+
+@author: djg
+'''
+
+class Fun():
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
+    def sayHello(self):
+        print "hello world!"
+        
+    def sayGoodbye(self):
+        print "goodbye cruel world..."
---
+++ python/src/com.bar.foo.pyground/com/bar/foo/pyground/main.py
@@ -0,0 +1,13 @@
+'''
+Created on Apr 22, 2015
+
+@author: djg
+'''
+
+import fun
+
+if __name__ == '__main__':
+    f = fun.Fun(None)
+    f.sayHello()
+    f.sayGoodbye()
+    
