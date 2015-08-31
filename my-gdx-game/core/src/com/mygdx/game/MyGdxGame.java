888e406584331c0751a0e094c3a8ed58f881506c
Jordan Deyton <jordan.deyton@gmail.com>
2015-08-30 21:08:34 -0400

    Disabling continuous rendering.
    However, it appears to not always call the ApplicationListener's
    resize() method when restoring a maximized window.

--- my-gdx-game/core/src/com/mygdx/game/MyGdxGame.java
+++ my-gdx-game/core/src/com/mygdx/game/MyGdxGame.java
@@ -8,47 +8,79 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture img;
+
+	/**
+	 * The batch used to update or render the sprite.
+	 */
+	private SpriteBatch spriteBatch;
+
+	/**
+	 * The sprite itself.
+	 */
 	private Sprite sprite;
+	/**
+	 * The dimensions of the (square) sprite.
+	 */
+	private float spriteSize;
+	/**
+	 * The sprite's texture.
+	 */
+	private Texture spriteTexture;
+	/**
+	 * The x-coordinate of the bottom-left corner of the sprite.
+	 */
+	private float spriteX;
+	/**
+	 * The y-coordinate of the bottom-right corner of the sprite.
+	 */
+	private float spriteY;
 
 	@Override
 	public void create() {
-		batch = new SpriteBatch();
-		img = new Texture("images/blue_sun_1.jpg");
-		sprite = new Sprite(img);
+		spriteBatch = new SpriteBatch();
+		spriteTexture = new Texture("images/blue_sun_1.jpg");
+		sprite = new Sprite(spriteTexture);
+
+		// Disable continuous rendering.
+		Gdx.graphics.setContinuousRendering(false);
+		Gdx.graphics.requestRendering();
+
+		return;
 	}
 
 	@Override
 	public void render() {
+
 		// Makes the background blue.
 		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		resizeSprite();
+		// Update the sprite.
+		spriteBatch.begin();
+		sprite.setBounds(spriteX, spriteY, spriteSize, spriteSize);
+		sprite.draw(spriteBatch);
+		spriteBatch.end();
 
 		return;
 	}
-	
-	private void resizeSprite() {
+
+	@Override
+	public void resize(int width, int height) {
+
 		// Size the sprite to 80% of the smaller of window width or height.
 		final int windowWidth = Gdx.graphics.getWidth();
 		final int windowHeight = Gdx.graphics.getHeight();
-		float spriteSize = windowWidth < windowHeight ? windowWidth : windowHeight;
-		spriteSize *= 0.8;
+		spriteSize = 0.8f * (windowWidth < windowHeight ? windowWidth : windowHeight);
 		// Get the location of the bottom-left corner of the sprite.
-		final float x = (windowWidth - spriteSize) * 0.5f;
-		final float y = (windowHeight - spriteSize) * 0.5f;
+		spriteX = (windowWidth - spriteSize) * 0.5f;
+		spriteY = (windowHeight - spriteSize) * 0.5f;
 
 		// Update the sprite's "camera" to adjust for the current screen size.
-		batch.getProjectionMatrix().setToOrtho2D(0f, 0f, windowWidth, windowHeight);
+		spriteBatch.getProjectionMatrix().setToOrtho2D(0f, 0f, windowWidth, windowHeight);
 
-		// Update the sprite.
-		batch.begin();
-		sprite.setBounds(x, y, spriteSize, spriteSize);
-		sprite.draw(batch);
-		batch.end();
-		
+		// We must request a render op if the app was resized.
+		Gdx.graphics.requestRendering();
+
 		return;
 	}
 }
